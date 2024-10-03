f3671d35329425d663ab91078558000b2754a4a0
andresF864 <159442816+andresF864@users.noreply.github.com>
2024-10-03 10:44:55 -0500

    Final

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -10,17 +10,22 @@ function Projects() {
       <div className="content">
         <p>Práctica HTML + Media Query-Pagina Responsive</p>
         <a href="https://github.com/andresF864/Tarea.git" className="subtitle">GitHub</a>
-        <img className="imge" src="" alt="Imagen de html básico" />
+        <img className="imge" src="https://preview.redd.it/trabajos-web-v0-ijkpiiz19ksd1.png?width=1871&format=png&auto=webp&s=35ed2e83fe6443277419ceedd911ee5a5224625b" alt="Imagen de html básico" />
       </div>
       <div className="content">
         <p>Formulario de Registro y Recuperación de Contraseñas UAO</p>
         <a href="https://github.com/edwinmgallego/clase-practica-css.git" className="subtitle">GitHub</a>
-        <img className="imge" src="" alt="Imagen del Formulario" />
+        <img className="imge" src="https://preview.redd.it/trabajos-web-v0-3ypodiz19ksd1.png?width=752&format=png&auto=webp&s=7a15cc69e8f41e307540f23df28d34114a8d9576" alt="Imagen del Formulario" />
       </div>
       <div className="content">
         <p>Numeros Iguales y Cual es el Mayor</p>
         <a href="https://github.com/andresF864/javascript-2.git" className="subtitle">GitHub</a>
-        <img className="imge" src="" alt="Imagen de la consola" />
+        <img className="imge" src="https://preview.redd.it/trabajos-web-v0-345p7jz19ksd1.png?width=663&format=png&auto=webp&s=b9d89e52688064785ebfed620a00247253c0b072" alt="Imagen de la consola" />
+      </div>
+      <div className="content">
+        <p>Cards</p>
+        <a href="https://github.com/andresF864/Cards.git" className="subtitle">GitHub</a>
+        <img className="imge" src="https://preview.redd.it/trabajos-web-v0-qfqi6fz19ksd1.png?width=1017&format=png&auto=webp&s=ab7ee0c9bcfd3084d5b497eeb50d92316bf5a64c" alt="Imagen de la consola" />
       </div>
     </div>
   );
