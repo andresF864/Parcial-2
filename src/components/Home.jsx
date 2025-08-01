952fc6e1d2fa9f6220a09e2ea5aec048f34491d1
andresF864 <159442816+andresF864@users.noreply.github.com>
2025-08-01 11:09:03 -0500

    css 2

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -38,7 +38,6 @@ function Home() {
         <p>Tel: 3116071850</p>
         <a href="https://www.instagram.com/andresf_864/" className="subtitle">Instagram</a>
         <br />
-        <br />
         <a href="https://www.linkedin.com/in/andres-de-la-cruz-ulloa-64846121b" className="subtitle">Linkedin</a>
       </div>
     </div>
