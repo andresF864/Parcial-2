2745c3d23b977874c1726845c392a709a21cf9d2
andresF864 <159442816+andresF864@users.noreply.github.com>
2025-08-01 11:12:20 -0500

    css3

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -37,7 +37,6 @@ function Home() {
         <p>Información de Contacto</p>
         <p>Tel: 3116071850</p>
         <a href="https://www.instagram.com/andresf_864/" className="subtitle">Instagram</a>
-        <br />
         <a href="https://www.linkedin.com/in/andres-de-la-cruz-ulloa-64846121b" className="subtitle">Linkedin</a>
       </div>
     </div>
