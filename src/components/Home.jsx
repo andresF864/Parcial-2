5f625f6f908faed4e738c3f5c99250ace8d45520
andresF864 <159442816+andresF864@users.noreply.github.com>
2025-08-01 10:57:58 -0500

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,26 +9,35 @@ function Home() {
       </div>
       <div className="content">
       <img className="imge" src="https://preview.redd.it/n5b2k59hhfgf1.png?width=317&format=png&auto=webp&s=70020fd3e17ce99bde7a85c9e518177e29eb2ad9" alt="Foto de Andres"/>
-        <p>Estudiante de ingeniería multimedia en la Universidad Autónoma de Occidente con experiencia en proyectos que integran diferentes tipos de metodologías de diseño e inclusión de contenidos diversos, tales como: imágenes, video, gráficos, sonido y animación.</p>
+        <p>Estudiante de ingeniería multimedia en la Universidad Autónoma de Occidente con experiencia en proyectos que integran diferentes tipos de metodologías de diseño e inclusión de diversos contenidos multimedia, tales como: imágenes, video, gráficos, sonido y animación.</p>
       </div>
       <div className="content2">
         <p>HARD SKILLS:</p>
-        <p>- Manejo del idioma inglés</p>
-        <p>- Adecuado manejo de la Suite de Adobe</p>
+        <p>- Manejo avanzado del idioma inglés</p>
+        <p>- Manejo adecuado  de la Suite de Adobe</p>
         <p>- Modelado 3D con texturizado y Render</p>
         <p>- Producción de contenido en Unity</p>
-        <p>- Producción Audiovisual</p>
+        <p>- Producción de contenido Audiovisual</p>
         <p>- Manejo de bases de datos SQL y NoSQL</p>
-        <p>- Diseño de páginas web</p>
+        <p>- Diseño web</p>
+        <p>- Diseño de experiencias multimedia</p>
       </div>
       <div className="content3">
         <p>SOFT SKILLS:</p>
-        <p>- Procesamiento de Señales</p>
+        <p>- Resolución de problemas</p>
+        <p>- Comunicación efectiva</p>
+        <p>- Creatividad</p>
+        <p>- Adaptabilidad</p>
+        <p>- Gestión del tiempo</p>
+        <p>- Paciencia</p>
+        <p>- Resiliencia</p>
+        <p>- Pensamiento crítico</p>
       </div>
       <div className="content4">
         <p>Información de Contacto</p>
         <p>Tel: 3116071850</p>
         <a href="https://www.instagram.com/andresf_864/" className="subtitle">Instagram</a>
+        <a href="https://www.linkedin.com/in/andres-de-la-cruz-ulloa-64846121b" className="subtitle">Linkedin</a>
       </div>
     </div>
   );
