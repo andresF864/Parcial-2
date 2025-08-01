4367959c1a156159318b5d28e498e91ce7219f75
andresF864 <159442816+andresF864@users.noreply.github.com>
2025-08-01 10:47:58 -0500

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,7 @@ function Home() {
         <h3>Andres Felipe De La Cruz Uloa</h3>
       </div>
       <div className="content">
-      <img className="imge" src="/img/Andres.jpg" alt="Foto de Andres"/>
+      <img src="/img/Andres.jpg" alt="Foto de Andres"/>
         <p>Estudiante de ingeniería multimedia en la Universidad Autónoma de Occidente con experiencia en proyectos que integran diferentes tipos de metodologías de diseño e inclusión de contenidos diversos, tales como: imágenes, video, gráficos, sonido y animación.</p>
       </div>
       <div className="content2">
